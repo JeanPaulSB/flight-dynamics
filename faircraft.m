a3ff9708a83f7e69d1c6e8f89f15719b45a0406e
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-03-05 10:10:19 -0500

    fix line 8

--- faircraft.m
+++ faircraft.m
@@ -2,9 +2,16 @@
 % function to define the mathematical model for a fixed wing aircraft
 function [xdot,y] = faircraft(t,x,delta,Vwe,deltaCGb,aircraft)
   % extract components of x and delta
-  pe = x(1:3,1);
-  Phi = x(4:6,1);
-  Vb = x(7:9,1); 
-  Omegab = (10:12,1);
+  % pe = x(1:3,1);
+  % Phi = x(4:6,1);
+  % Vb = x(7:9,1); 
+  % Omegab = x(10:12,1);
+
+  xdot = 3
+  y = 4
   
 end
+
+
+
+
